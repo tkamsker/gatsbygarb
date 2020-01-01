4d477524e0cf70672ff4e406b347f66b47b3e628
Thomas Kamsker <tkamsker@tkamsker.at>
2020-01-01 15:44:01 +0100

    Wechesl Contentful auf Strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,6 @@ module.exports = {
   resolve: 'gatsby-source-contentful',
   options: {
     spaceId: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   },
 }
